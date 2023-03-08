491273c03e8e5abe91934cb4d9ddb292b28ebd3f
Mahabub Alam <rjmahabub543@gmail.com>
2023-03-08 09:48:33 +0600

    create development branch

---
+++ Components/pages/Home/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="py-6 lg:py-0 lg:h-screen bg-green-200 grid-place-center">
+      <h2>this is header</h2>
+      <p>development branch added</p>
+    </header>
+  );
+};
+
+export default Header;
