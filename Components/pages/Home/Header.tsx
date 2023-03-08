f57969d20c9a8a02bf3cb53f0498e14d1cde9dd6
Mahabub Alam <rjmahabub543@gmail.com>
2023-03-08 09:51:46 +0600

    hompage branch added

---
+++ Components/pages/Home/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="py-6 lg:py-0 lg:h-screen bg-green-200 grid-place-center">
+      <h2>this is header</h2>
+      <p>development branch added</p>
+      <p>homepage branch added</p>
+    </header>
+  );
+};
+
+export default Header;
