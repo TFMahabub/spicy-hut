61739d9fcd0a3de70c3260cfddf5aa58361a688b
Mahabub Alam <rjmahabub543@gmail.com>
2023-03-09 22:08:32 +0600

    Perfect Dining Experience section done

--- Components/layouts/MainLayout.tsx
+++ Components/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import Navbar from "../shared/navbar/Navbar";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative">
+    <div className="relative max-w-[1488px] mx-auto">
       <Navbar />
       {children}
     </div>
