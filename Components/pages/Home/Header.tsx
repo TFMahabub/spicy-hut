38275d7fab43342215129e9e21932423a6ece0d4
Mahabub Alam <rjmahabub543@gmail.com>
2023-03-09 18:36:58 +0600

    banner design complte

--- Components/pages/Home/Header.tsx
+++ Components/pages/Home/Header.tsx
@@ -1,11 +1,17 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <header className="py-6 lg:py-0 lg:h-screen bg-green-200 grid-place-center">
-      <h2>this is header</h2>
-      <p>development branch added</p>
-      <p>homepage branch added</p>
-      <p>check push to homepage branch</p>
-      <p>another check</p>
+    <header className="h-screen bg-banner_backbround_image bg-no-repeat bg-center bg-black">
+      <div className="bg-bodyBackground/40 w-full h-full grid_place_center">
+        <div className="container grid_place_center space-y-md">
+          <h2 className="banner_title_text font-cormorant text-secondary uppercase">Contemporary <br /> Japanese Cuisine</h2>
+          <div className="flex flex-col md:flex-row gap-sm">
+            <button className="primary_button "><Link href={"/"}>Make A ReserVation</Link></button>
+            <button className="primary_button"><Link href={"/"}>Make A ReserVation</Link></button>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import FoundedIn1998 from "@/Components/pages/Home/FoundedIn1998/FoundedIn1998";
 import Header from "@/Components/pages/Home/Header";
 
 export default function Home() {
   return (
-    <>
+    <div className="space-y-[7rem] absolute top-0 w-full">
       <Header />
-    </>
+      <FoundedIn1998/>
+    </div>
   );
 }
