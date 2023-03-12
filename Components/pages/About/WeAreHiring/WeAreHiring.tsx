392c86a98511a2a2cf14e71f4a241ffbe989bcc7
Mahabub Alam <rjmahabub543@gmail.com>
2023-03-12 15:34:29 +0600

    check merge

--- Components/pages/About/WeAreHiring/WeAreHiring.tsx
+++ Components/pages/About/WeAreHiring/WeAreHiring.tsx
@@ -20,10 +20,7 @@ const WeAreHiring = () => {
           />
           <p className="pText">
             We are looking for chefs and service staff at Patio.Time Sushi and
-            our other branches. Please call or email for details. Lorem ipsum,
-            dolor sit amet consectetur adipisicing elit. Odit perferendis id
-            voluptas incidunt consequatur nam tempore quos eius dolore
-            perspiciatis.
+            our other branches. Please call or email for details. Lorem ipsum dolor sit amet consectetur adipisicing elit. A soluta deleniti quibusdam dolorum est corporis quis reprehenderit praesentium facilis possimus reiciendis autem atque, fugiat natus sit nemo alias. Eum, adipisci.
           </p>
           <div className="space-y-2">
             <p className="tracking-wide text-lg leading-relaxed mt-3 text-secondary/60">
