991cba7a8c9b5d42e4e0c7577f7f8a1828a2d2c1
Mahabub Alam <rjmahabub543@gmail.com>
2023-03-12 22:24:58 +0600

    button update at homepage

--- Components/pages/Home/ViewFullMenus/ViewFullMenus.tsx
+++ Components/pages/Home/ViewFullMenus/ViewFullMenus.tsx
@@ -3,14 +3,13 @@ import viewFullMenus2 from "@/public/assats/home(view-full-menu2).jpg";
 import viewFullMenus3 from "@/public/assats/home(view-full-menu3).jpg";
 import viewFullMenus4 from "@/public/assats/home(view-full-menu4).jpg";
 import Image from "next/image";
-import Link from "next/link";
 
 const ViewFullMenus = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       <Image src={viewFullMenus1} className="" alt="" />
       <div className="grid_place_center">
-        <div className="py-16 lg:py-0 px-2 lg:px-8">
+        <div className="py-16 lg:py-0 px-2 lg:px-8 flex flex-col items-center">
           <div className={`flex gap-4 items-center justify-center`}>
             <hr className={`w-10 h-[2px] bg-primary block`} />
             <h2 className="top_title">À LA CARTE</h2>
@@ -23,16 +22,12 @@ const ViewFullMenus = () => {
             We serve food, harmony, and laughter. Making delicious food and
             providing a wonderful eating experience. update something
           </p>
-          <Link href={"/"}>
-            <p className="text-secondary text-center tracking-wide text-lg leading-relaxed mt-8 hover:underline hover:underline-offset-8">
-              BROWSE MENUS
-            </p>
-          </Link>
+          <a href="http://" target="_blank" rel="noopener noreferrer" className="mt-5 button_border_animation_end_to_start text-secondary after:bg-primary text-center">BROWSE MENUS</a>
         </div>
       </div>
       <Image src={viewFullMenus2} className="" alt="" />
       <div className="grid_place_center">
-        <div className="py-16 lg:py-0 px-2 lg:px-8">
+        <div className="py-16 lg:py-0 px-2 lg:px-8 flex flex-col items-center">
           <div className={`flex gap-4 items-center justify-center`}>
             <hr className={`w-10 h-[2px] bg-primary block`} />
             <h2 className="top_title">THE MOST POPULAR</h2>
@@ -45,11 +40,7 @@ const ViewFullMenus = () => {
             We present delicious and enticing dishes with high-quality
             ingredients. There is no set protocol for ordering from the menu.
           </p>
-          <Link href={"/"}>
-            <p className="text-secondary text-center tracking-wide text-lg leading-relaxed mt-8 hover:underline hover:underline-offset-8">
-              LEARN MORE
-            </p>
-          </Link>
+          <a href="http://" target="_blank" rel="noopener noreferrer" className="mt-5 button_border_animation_end_to_start text-secondary after:bg-primary text-center">LEARN MORE</a>
         </div>
       </div>
       <Image src={viewFullMenus3} className="" alt="" />
