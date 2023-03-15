ec4ec6595dec86d9fe31f23cd2e0c2017036201d
Mahabub Alam <rjmahabub543@gmail.com>
2023-03-15 14:51:51 +0600

    add menupage

--- Components/Cart/MenuCartGridView.tsx
+++ Components/Cart/MenuCartGridView.tsx
@@ -6,6 +6,7 @@ const MenuCartGridView = () => {
         <div className='border-[1px] border-secondary/30 shadow-secondary overflow-hidden rounded-lg text-secondary bg-[#000]'>
             <Image src={cardImage} className="" alt="" />
             <h3>this is cart</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, quos.</p>
         </div>
     );
 };
