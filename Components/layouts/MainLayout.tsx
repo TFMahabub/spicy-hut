2883d4a6d32a9daa49cad17628c086f784e95a02
Mahabub Alam <rjmahabub543@gmail.com>
2023-03-18 00:39:10 +0600

    Search icon functionality and component done

--- Components/layouts/MainLayout.tsx
+++ Components/layouts/MainLayout.tsx
@@ -1,12 +1,21 @@
+import { useSearchComponent } from "@/Components/hook/useAllState";
+import SearchModalIndex from "../modal/SearchModal/SearchModalIndex";
 import Footer from "../shared/footer/Footer";
 import Navbar from "../shared/navbar/Navbar";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { searchComponent } = useSearchComponent();
+  console.log(searchComponent)
+  // const searchComponent = false;
   return (
     <div className="relative">
       <Navbar />
       {children}
       <Footer />
+      {
+        searchComponent &&
+        <SearchModalIndex />
+      }
     </div>
   );
 };
