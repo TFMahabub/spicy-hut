06fbe2e1e076389edc87f264ff821685a2d311ee
Mahabub Alam <rjmahabub543@gmail.com>
2023-04-22 23:08:44 +0600

    deploy error solved-1

--- Components/hook/useFecting.ts
+++ Components/hook/useFecting.ts
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export const useFetch = async(fetchUrl:string) =>{
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    useEffect(()=>{
-        setLoading(true);
-        fetch(url)
-        .then(res=>res.json())
-        .then(data=>{
-            setData(data);
-            setLoading(false);
-        })
-        .catch(err=>{
-            setError(err);
-        })
-        .finally(()=>{
-            setLoading(false)
-        })
-    },[fetchUrl])
-    return {data, loading, error}
-}
+// export const useFetch = async(fetchUrl:string) =>{
+//     const [data, setData] = useState(null);
+//     const [loading, setLoading] = useState(false);
+//     const [error, setError] = useState(null);
+//     useEffect(()=>{
+//         setLoading(true);
+//         fetch(url)
+//         .then(res=>res.json())
+//         .then(data=>{
+//             setData(data);
+//             setLoading(false);
+//         })
+//         .catch(err=>{
+//             setError(err);
+//         })
+//         .finally(()=>{
+//             setLoading(false)
+//         })
+//     },[fetchUrl])
+//     return {data, loading, error}
+// }
