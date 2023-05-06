a9c20e600e8577ca0ec8ad35f572d17bb74cb6e1
Mahabub Alam <rjmahabub543@gmail.com>
2023-05-06 23:45:20 +0600

    create slice for menus categories and implement that

--- Components/pages/Menu/MenuIndex.tsx
+++ Components/pages/Menu/MenuIndex.tsx
@@ -1,18 +1,21 @@
-import { useMenuCategory } from '@/Components/hook/useAllState';
 import SectionTitle from '@/Components/reusedComponents/SectionTitle';
+import { setMenuCategories } from '@/Redux/menuPageSlice/menuPageSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import CartParent from './CartParent';
 
 const MenuIndex = () => {
-    const { menuCategory, setMenuCategory } = useMenuCategory();
-    // console.log(menuCategory);
+    //redux--------------------------
+    const { menusCategories } = useSelector((state: any) => state.menuPageSlice)
+    const dispatch = useDispatch();
+
     const menuItems = [
-        { name: "All", value: "all" },
-        { name: "Appetizer", value: "appetizer" },
-        { name: "Nigiri Sushi", value: "nigiri-sushi" },
-        { name: "Popular Sashimi", value: "popular-sashimi" },
-        { name: "Selected Sake", value: "selected-sake" },
-        { name: "Seasonal Specials", value: "seasonal-specials" },
-        { name: "Special", value: "specials" },
+        { name: "all", value: "all" },
+        { name: "global", value: "global" },
+        { name: "appetizer", value: "appetizer" },
+        { name: "nigri sushi", value: "nigri sushi" },
+        { name: "popular sashimi", value: "popular sashimi" },
+        { name: "selected sake", value: "selected sake" },
+        { name: "specials", value: "specials" }
     ]
     return (
         <div className='container section_gap'>
@@ -22,8 +25,12 @@ const MenuIndex = () => {
             <div className='mt-10'>
                 <div className='flex flex-col flex-wrap md:flex-row md:items-center md:justify-center gap-3'>
                     {
-                        menuItems?.map((item, i) => (
-                            <button key={i} onClick={() => setMenuCategory(item?.value)} className={`${menuCategory === item?.value ? "menuItem_button_fill" : "menuItem_button_outLine"}  duration-300`}>{item?.name}</button>
+                        menuItems?.map((item) => (
+                            <button
+                                key={item.name}
+                                onClick={() => dispatch(setMenuCategories(item?.value))} className={`${menusCategories === item?.value ? "menuItem_button_fill" : "menuItem_button_outLine"}  duration-300 capitalize`}>
+                                {item?.name}
+                            </button>
                         ))
                     }
                 </div>
