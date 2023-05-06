3fed35b204cacb970125b28d8757929e8f764eab
Mahabub Alam <rjmahabub543@gmail.com>
2023-05-06 19:04:40 +0600

    create to reUseAbleComponents

--- Components/pages/About/Image/DemoImage.tsx
+++ Components/pages/About/Image/DemoImage.tsx
@@ -1,3 +1,5 @@
+import DescriptionTextLG from "@/Components/reusedComponents/DescriptionTextLG";
+import TitleTextLarge from "@/Components/reusedComponents/TitleTextLarge";
 import Image from "next/image";
 
 const DemoImage = () => {
@@ -10,7 +12,12 @@ const DemoImage = () => {
         layout="fill"
         alt=""
       />
-      <div className="absolute inset-0 bg-[rgba(0,0,0,0.7)]"></div>
+      <div className="absolute inset-0 bg-[rgba(0,0,0,0.8)] flex items-center justify-center">
+        <div className="container flex flex-col text-center max-w-2xl space-y-2">
+          <TitleTextLarge >We're Always Ready for you</TitleTextLarge>
+          <DescriptionTextLG>We continue to be one of the top Japanese restaurants in America, attracting countless food lovers from all over the world. Our one-of-a-kind menu includes authentic tapas-style Japanese dishes and homemade desserts. new update</DescriptionTextLG>
+        </div>
+      </div>
     </div>
   );
 };
